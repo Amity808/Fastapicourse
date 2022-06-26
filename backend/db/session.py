24dec6bc0b22b555a621317e7ecbfe34dd06d39e
Amity <bolarinwamuhdsodiq0@gmail.com>
2022-06-26 20:37:47 +0100

    Connect to database

---
+++ backend/db/session.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+#from core.config import settings
+
+#SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
+engine = create_engine(SQLALCHAMY_DATABASE_URL,
+                       connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
